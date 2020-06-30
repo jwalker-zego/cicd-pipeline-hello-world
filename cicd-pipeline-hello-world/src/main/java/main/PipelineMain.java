5616ab4101a49351f31931209e55a5eecb40cc75
jwalker <jwalker@192.168.0.4>
2020-06-30 15:31:29 -0700

    Updated pipeline main class

--- cicd-pipeline-hello-world/src/main/java/main/PipelineMain.java
+++ cicd-pipeline-hello-world/src/main/java/main/PipelineMain.java
@@ -4,6 +4,6 @@ public class PipelineMain
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello from the CICD Pipeline");
+		System.out.println("Hi there from the CICD Pipeline");
 	}
 }
