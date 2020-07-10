aaab2bc9f5f266617672c91354217e5a9de1cee4
jwalker <jwalker@192.168.0.4>
2020-07-09 23:20:37 -0700

    Provided support for spring integration tests (mvn verify):
    mvn verify:
    1) Build web app
    2) Build image
    3) Run integration tests

---
+++ cicd-pipeline-hello-world/src/test/java/cicd/qe/service/SpringBootFruitTests.java
@@ -0,0 +1,13 @@
+package cicd.qe.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitAPITests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
